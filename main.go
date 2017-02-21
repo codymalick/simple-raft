6126600ebc0529065b1ebfcc40cca5be2bf360bc
codymalick <cody.malick@gmail.com>
2017-02-20 21:30:01 -0800

    Added initial files

---
+++ main.go
@@ -0,0 +1,11 @@
+// Simple implementation of the raft concensus algorithm using golang rpc
+// Cody Malick and Jacob Broderick
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Println("Test")
+}
