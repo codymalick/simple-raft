ac6f7e92ba7cc16f0aabfaec6616de26a1ae7714
codymalick <cody.malick@gmail.com>
2017-03-13 23:53:19 -0700

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
-./simple-raft -id 0 -port 50000 -state 0
-./simple-raft -id 0 -port 50001
-./simple-raft -id 0 -port 50002
-./simple-raft -id 0 -port 50003
-./simple-raft -id 0 -port 50004
+./simple-raft -id 0 -port :50000 -state 0
+./simple-raft -id 0 -port :50001
+./simple-raft -id 0 -port :50002
+./simple-raft -id 0 -port :50003
+./simple-raft -id 0 -port :50004
